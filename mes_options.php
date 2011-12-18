4462580f12446752ca7a0244420f6a7619e2d5fa
Cedric <cedric@yterium.com>
2011-12-18 16:59:47 +0100

    autorisations

--- mes_options.php
+++ mes_options.php
@@ -3,3 +3,11 @@
 function forum_recuperer_titre($objet, $id_objet, $id_forum=0, $publie = false) {
 	return forum_recuperer_titre_dist($objet,$id_objet,$id_forum,$publie);
 }
+
+
+function autoriser_ajoutertags($faire, $type, $id, $qui, $opt){
+	return $qui['statut']=='0minirezo';
+}
+function autoriser_supprimertags($faire, $type, $id, $qui, $opt){
+	return $qui['statut']=='0minirezo';
+}
